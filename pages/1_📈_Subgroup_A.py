1bb65ccdb123c69cfde2422dc766677681d0bd06
Ong-Jing-Jun <E0957685@u.nus.edu>
2024-11-06 11:57:48 +0800

    Add Streamlit files

--- pages/1_📈_Subgroup_A.py
+++ pages/1_📈_Subgroup_A.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from tabs.tab2a import load_data_jj, display_tab2a, display_tab2b, display_tab2c
 
 def main():
     """Main function to run the Streamlit app."""
@@ -7,6 +8,10 @@ def main():
 
     tab1, tab2, tab3 = st.tabs(["🔍Customer Analysis", "📉 Customer Churn Rates","📬Marketing Channel Analysis"])
     
-    
+    df_jj = load_data_jj()
+    # Display content for tab2
+    display_tab2a(tab2, df_jj)
+    display_tab2b(tab2, df_jj)
+    display_tab2c(tab2, df_jj)
 if __name__ == "__main__":
     main()
